c60e0cc58c3c85d91b952e09a2fd1d03611063cd
woolflare <167610549+woolflare@users.noreply.github.com>
2024-05-20 08:19:31 +0800

    Update dynla.sh

--- dynla.sh
+++ dynla.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 
+if [[ -t 1 ]]
+then
+  tty_escape() { printf "\033[%sm" "$1"; }
+else
+  tty_escape() { :; }
+fi
+tty_mkbold() { tty_escape "1;$1"; }
+tty_underline="$(tty_escape "4;39")"
+tty_blue="$(tty_mkbold 34)"
+tty_red="$(tty_mkbold 31)"
+tty_green="$(tty_mkbold 32)"
+tty_bold="$(tty_mkbold 39)"
+tty_reset="$(tty_escape 0)"
+
 if [ -t 0 ]; then
     echo "Running in interactive mode."
 else
-    echo "----------------------------------------------------"
+    echo "==================================================="
+    echo ""
     echo "This script is not running in an interactive mode."
-    echo "----------------------------------------------------"
+    echo ""
+    echo "==================================================="
+    echo ""
     echo "Please download the script and run it locally for interactive features."
     echo "Use the following commands to download and run the script:"
     echo ""
@@ -35,15 +52,15 @@ if [ -z "$token" ]; then
 
     echo "Open the following URL in your browser"
     echo ""
-    echo "$verification_uri"
+    echo "${tty_underline}$verification_uri${tty_reset}"
     echo ""
     echo "and enter the user code"
     echo ""
-    echo "$user_code"
+    echo "${tty_green}$user_code${tty_reset}"
     echo ""
     echo "to complete the login."
-
     echo "Waiting for user to complete login..."
+
 fi
 while [ -z "$token" ]; do
     sleep "$interval"
@@ -75,7 +92,6 @@ done
 
 login_response=$(curl -s "https://beta.dyn.la/oauth/callback?provider=github" \
 -H "Authorization: Bearer $token")
-
 login_token=$(echo "$login_response" | grep -o '"token":"[^"]*"' | sed -e 's/^"token":"//' -e 's/"$//')
 username=$(echo "$login_response" | grep -o '"username":"[^"]*"' | sed -e 's/^"username":"//' -e 's/"$//')
 login_error=$(echo "$login_response" | grep -o '"error":"[^"]*"' | sed -e 's/^"error":"//' -e 's/"$//')
@@ -94,30 +110,31 @@ ______   __   __  __    _        ___      _______
 echo ""
 echo "Welcome $username"
 echo "-----------------"
+echo ""
 
 if [ -z "$DYNLA" ]; then
-    echo ""
     echo "Use this command to set an environment variable for login persistence"
-    echo "export DYNLA=$token"
+    echo "${tty_blue}export DYNLA=$token${tty_reset}"
     echo ""
 fi
 
 while true; do
-    echo ""
     echo "Choose an option:"
     echo ""
-    echo "n - Create new dynamic DNS"
-    echo "d - Delete dynamic DNS"
-    echo "r - Reset dynamic DNS"
-    echo "l - List all dynamic DNS"
-    echo "e - Exit"
-    echo "h - Display help"
+    echo "${tty_green}n${tty_reset} - Create new dynamic DNS"
+    echo "${tty_green}d${tty_reset} - Delete dynamic DNS"
+    echo "${tty_green}r${tty_reset} - Reset dynamic DNS password"
+    echo "${tty_green}l${tty_reset} - List all dynamic DNS"
+    echo "${tty_green}e${tty_reset} - Exit"
+    echo "${tty_green}h${tty_reset} - Display help"
     echo ""
     read -p "Enter option code: " option
     echo ""
 
     case "$option" in
         n)
+            echo "[Creatie new dynamic DNS]"
+            echo ""
             read -p "Enter hostname e.g test.dyn.la: " hostname
             echo ""
             case "$hostname" in
@@ -127,11 +144,13 @@ while true; do
             response=$(curl -s -X POST "https://beta.dyn.la/new" \
             -H "Authorization: Bearer $login_token" \
             -d "hostname=$hostname")
-            echo "-> $response"
+            echo "${tty_blue}->${tty_reset} $response"
             echo ""
-            read -p "Press enter to continue..."
+            # read -p "Press enter to continue..."
             ;;
         d)
+            echo "${tty_red}[Delete dynamic DNS]${tty_reset}"
+            echo ""
             read -p "Enter hostname e.g test.dyn.la: " hostname
             echo ""
             case "$hostname" in
@@ -141,11 +160,13 @@ while true; do
             response=$(curl -s -X POST "https://beta.dyn.la/delete" \
             -H "Authorization: Bearer $login_token" \
             -d "hostname=$hostname")
-            echo "-> $response"
+            echo "${tty_blue}->${tty_reset} $response"
             echo ""
-            read -p "Press enter to continue..."
+            # read -p "Press enter to continue..."
             ;;
         r)
+            echo "[Reset dynamic DNS password]"
+            echo ""
             read -p "Enter hostname e.g test.dyn.la: " hostname
             echo ""
             case "$hostname" in
@@ -155,16 +176,18 @@ while true; do
             response=$(curl -s -X POST "https://beta.dyn.la/reset" \
             -H "Authorization: Bearer $login_token" \
             -d "hostname=$hostname")
-            echo "-> $response"
+            echo "${tty_blue}->${tty_reset} $response"
             echo ""
-            read -p "Press enter to continue..."
+            # read -p "Press enter to continue..."
             ;;
         l)
+            echo "[List all dynamic DNS]"
+            echo ""
             response=$(curl -s -X POST "https://beta.dyn.la/list" \
             -H "Authorization: Bearer $login_token")
-            echo "-> $response"
+            echo "${tty_blue}->${tty_reset} $response"
             echo ""
-            read -p "Press enter to continue..."
+            # read -p "Press enter to continue..."
             ;;
         h)
             echo "Usage Instructions:"
@@ -184,22 +207,22 @@ while true; do
             echo "1. Update hostname using POST method:"
             echo "   curl -X POST https://dns.dyn.la/update -d \"password=iz5aqj11p8mual4e\" -d \"hostname=test.dyn.la\""
             echo ""
-            read -p "Press enter to continue..."
+            # read -p "Press enter to continue..."
             ;;
         e)
-            echo "-> Exiting."
+            echo "${tty_blue}->${tty_reset} Exiting."
             break
             ;;
         *)
-            echo "-> Undefined option."
+            echo "${tty_blue}->${tty_reset} Undefined option."
             ;;
     esac
 done
 else
     echo ""
-    echo "Error"
+    echo "${tty_red}Error${tty_reset}"
     echo ""
-    echo "-> $login_error"
+    echo "${tty_blue}->${tty_reset} $login_error"
     echo ""
     echo "This might be caused by invalid or expired login credentials."
     echo "If environment variables are set, unset login credentials with 'unset DYNLA' and try again."
